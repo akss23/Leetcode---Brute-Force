7157b6c108d1a035d257ebdca29a8ba790444ef0
Akshat Parashar <156771282+akss23@users.noreply.github.com>
2025-06-15 00:17:19 +0530

    Create Smallest_Divisor.cpp

---
+++ Smallest_Divisor.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int max= *max_element(nums.begin(),nums.end());
+        
+        for(int i=1;i<=max;i++){
+            int cnt=0;
+            for(int j=0;j<nums.size();j++){
+               cnt+= ceil(double(nums[j])/i);
+            }
+            if(cnt<=threshold) return i;
+            
+            
+        }
+        return -1;
+    }
+}; Added brute force solution for Smallest Divisor problem
